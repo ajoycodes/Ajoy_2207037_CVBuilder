b6440f25c82e9f6a65fbf7def91415c69703e593
Ajoy Saha <138325582+ajoycodes@users.noreply.github.com>
2025-11-25 07:34:14 +0600

    Quick fixes for adding database

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -8,10 +8,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage st) throws Exception {
-        FXMLLoader l = new FXMLLoader(getClass().getResource("welcome.fxml"));
+        FXMLLoader l = new FXMLLoader(getClass().getResource("/com/example/demo/welcome.fxml"));
+
+
         Scene sc = new Scene(l.load());
         st.setTitle("Welcome");
         st.setScene(sc);
+        st.setMaximized(true);
         st.show();
     }
 }
